0c1dbc1e815de97859aa3b5e67e54f4d97040001
bromble73 <92156988+bromble73@users.noreply.github.com>
2021-10-16 17:37:55 +0300

    Add files via upload

---
+++ innerclasses/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CellPhone phone = new CellPhone("SAMSUNG", "S10");
+        phone.turnOn();
+        phone.call("1234567890");
+
+    }
+}
